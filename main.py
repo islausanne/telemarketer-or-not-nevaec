f4ce80314988060d268ec41c08ed01a286a42bf9
nevaec <ncoughlan@islstudent.ch>
2025-08-28 14:44:09 +0200

    Solution

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # You solution goes here
+num1 = int(input(""))
+num2 = int(input(""))
+num3 = int(input(""))
+num4 = int(input(""))
+
+if (num1 == 8 or num1 == 9) and num2 == num3 and (num4 == 8 or num4 == 9):
+    print("ignore")
+else:
+    print("answer")
